dbcb455e561fb6debc7ce481bcfe30ec5f748454
navono <navono007@gmail.com>
2018-09-06 15:00:59 +0800

    seperate files

--- stringsvc1/main.go
+++ stringsvc1/main.go
@@ -2,76 +2,11 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"errors"
 	"log"
 	"net/http"
-	"strings"
 
-	"github.com/go-kit/kit/endpoint"
 	httptransport "github.com/go-kit/kit/transport/http"
 )
-
-// StringService define the business logic
-type StringService interface {
-	Uppercase(context.Context, string) (string, error)
-	Count(context.Context, string) int
-}
-
-type stringService struct{}
-
-func (stringService) Uppercase(_ context.Context, s string) (string, error) {
-	if s == "" {
-		return "", ErrEmpty
-	}
-	return strings.ToUpper(s), nil
-}
-
-func (stringService) Count(_ context.Context, s string) int {
-	return len(s)
-}
-
-// ErrEmpty is returned when input string is empty
-var ErrEmpty = errors.New("Empty string")
-
-// Requests and response for RPC
-type uppercaseRequest struct {
-	S string `json:"s"`
-}
-
-type uppercaseResponse struct {
-	V   string `json:"v"`
-	Err string `json:"err,omitempty"` // error don't JSON-marshal
-}
-
-type countRequest struct {
-	S string `json:"s"`
-}
-
-type countResponse struct {
-	V int `json:"v"`
-}
-
-// Endpoints
-// an endpoint represents a single RPC, a single method in service
-func makeUppercaseEndpoint(svc StringService) endpoint.Endpoint {
-	return func(ctx context.Context, request interface{}) (interface{}, error) {
-		req := request.(uppercaseRequest)
-		v, err := svc.Uppercase(ctx, req.S)
-		if err != nil {
-			return uppercaseResponse{v, err.Error()}, nil
-		}
-		return uppercaseResponse{v, ""}, nil
-	}
-}
-
-func makeCountEndpoint(svc StringService) endpoint.Endpoint {
-	return func(ctx context.Context, request interface{}) (interface{}, error) {
-		req := request.(countRequest)
-		v := svc.Count(ctx, req.S)
-		return countResponse{v}, nil
-	}
-}
 
 type ctxManager struct {
 	name string
@@ -98,25 +33,4 @@ func main() {
 	http.Handle("/uppercase", uppercaseHandler)
 	http.Handle("/count", countHandler)
 	log.Fatal(http.ListenAndServe(":8088", nil))
-}
-
-func decodeUppercaseRequest(ctx context.Context, r *http.Request) (interface{}, error) {
-	var request uppercaseRequest
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		return nil, err
-	}
-	return request, nil
-}
-
-func decodeCountRequest(_ context.Context, r *http.Request) (interface{}, error) {
-	var request countRequest
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		return nil, err
-	}
-	return request, nil
-}
-
-func encodeResponse(ctx context.Context, w http.ResponseWriter, response interface{}) error {
-	log.Println(ctx.Value(ctxManager{"request"}))
-	return json.NewEncoder(w).Encode(response)
 }
